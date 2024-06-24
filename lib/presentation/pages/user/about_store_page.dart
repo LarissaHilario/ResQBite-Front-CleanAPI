febfa35d8108e2ad8cb991f77042b1c682c25fcb
LarissaHilario <larihid1@gmail.com>
2024-06-24 14:43:18 -0600

    fixed scroll in page

--- lib/presentation/pages/user/about_store_page.dart
+++ lib/presentation/pages/user/about_store_page.dart
@@ -15,68 +15,89 @@ class _StoreUserPageState extends State<StoreUserPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Container(
-          height: 205,
-          child: Image.asset(
-            'assets/images/photo_store.png',
-            width: double.infinity,
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Container(
+            height: 205,
+            child: Image.asset(
+              'assets/images/photo_store.png',
+              width: double.infinity,
+            ),
           ),
-        ),
-        const Align(
+          const Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+                child: Text(
+                  'Pastelería Sofi',
+                  style: TextStyle(
+                    fontSize: 26.0,
+                    color: Color(0xFF464646),
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'FiraSansCondensed',
+                    letterSpacing: 5,
+                  ),
+                ),
+              )),
+          Align(
             alignment: Alignment.topLeft,
             child: Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-              child: const Text(
-                'Pastelería Sofi',
-                style: TextStyle(
-                  fontSize: 26.0,
-                  color: Color(0xFF464646),
-                  fontWeight: FontWeight.w500,
-                  fontFamily: 'FiraSansCondensed',
-                  letterSpacing: 5,
-                ),
-              ),
-            )),
-        Align(
-          alignment: Alignment.topLeft,
-          child: Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20),
-              child: Column(children: [
-                Row(children: [
-                  Align(
-                      alignment: Alignment.topLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: SvgPicture.asset(
-                          'assets/images/location_icon.svg',
-                          width: 25,
-                        ),
-                      )),
-                  const Text(
-                    'Col. Oriente Sur #122',
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      color: Color(0xFF464646),
-                      fontWeight: FontWeight.w500,
-                      fontFamily: 'FiraSansCondensed',
-                      letterSpacing: 2,
-                    ),
-                  ),
-                ]),
-                Row(children: [
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Column(children: [
                   Row(children: [
                     Align(
                         alignment: Alignment.topLeft,
                         child: Padding(
-                          padding: const EdgeInsets.only(top: 2, left: 5),
+                          padding: const EdgeInsets.only(top: 10),
                           child: SvgPicture.asset(
-                            'assets/images/clock.svg',
+                            'assets/images/location_icon.svg',
+                            width: 25,
+                          ),
+                        )),
+                    const Text(
+                      'Col. Oriente Sur #122',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Color(0xFF464646),
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'FiraSansCondensed',
+                        letterSpacing: 2,
+                      ),
+                    ),
+                  ]),
+                  Row(children: [
+                    Row(children: [
+                      Align(
+                          alignment: Alignment.topLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 2, left: 5),
+                            child: SvgPicture.asset(
+                              'assets/images/clock.svg',
+                              width: 18,
+                            ),
+                          )),
+                      const Text(
+                        ' 09:00 hrs - 19:00 hrs',
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          color: Color(0xFF464646),
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'FiraSansCondensed',
+                          letterSpacing: 1,
+                        ),
+                      ),
+                    ]),
+                    Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 1, left: 40),
+                          child: SvgPicture.asset(
+                            'assets/images/phone.svg',
                             width: 18,
                           ),
                         )),
                     const Text(
-                      ' 09:00 hrs - 19:00 hrs',
+                      ' 9514134607',
                       style: TextStyle(
                         fontSize: 14.0,
                         color: Color(0xFF464646),
@@ -85,94 +106,74 @@ class _StoreUserPageState extends State<StoreUserPage> {
                         letterSpacing: 1,
                       ),
                     ),
-                  ]),
-                  Align(
-                      alignment: Alignment.topLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 1, left: 40),
-                        child: SvgPicture.asset(
-                          'assets/images/phone.svg',
-                          width: 18,
-                        ),
-                      )),
-                  const Text(
-                    ' 9514134607',
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      color: Color(0xFF464646),
-                      fontWeight: FontWeight.w500,
-                      fontFamily: 'FiraSansCondensed',
-                      letterSpacing: 1,
-                    ),
+                  ])
+                ])),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10, top: 20),
+              child: Container(
+                  height: 230,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color(0xFF88B04F).withOpacity(.21),
                   ),
-                ])
-              ])),
-        ),
-        Align(
-          alignment: Alignment.topCenter,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10, top: 20),
-            child: Container(
-                height: 230,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: const Color(0xFF88B04F).withOpacity(.21),
-                ),
-                child: Column(
-                  children: [
-                    const Align(
-                        alignment: Alignment.topLeft,
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 25, top: 10),
-                          child: Text(
-                            'OFERTAS',
-                            style: TextStyle(
-                              fontSize: 25.0,
-                              color: Color(0xFF464646),
-                              fontWeight: FontWeight.w500,
-                              fontFamily: 'FiraSansCondensed',
-                              letterSpacing: 3,
+                  child: Column(
+                    children: [
+                      const Align(
+                          alignment: Alignment.topLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 25, top: 10),
+                            child: Text(
+                              'OFERTAS',
+                              style: TextStyle(
+                                fontSize: 25.0,
+                                color: Color(0xFF464646),
+                                fontWeight: FontWeight.w500,
+                                fontFamily: 'FiraSansCondensed',
+                                letterSpacing: 3,
+                              ),
                             ),
+                          )),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: 4,
+                            itemBuilder: (BuildContext context, int index) {
+                              return const Padding(
+                                padding: EdgeInsets.all(10.0),
+                                child: CardOfferComponent(),
+                              );
+                            },
                           ),
-                        )),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: 4,
-                          itemBuilder: (BuildContext context, int index) {
-                            return const Padding(
-                              padding: EdgeInsets.all(10.0),
-                              child: CardOfferComponent(),
-                            );
-                          },
                         ),
                       ),
-                    ),
-
-                  ],
-                )),
+                    ],
+                  )),
+            ),
           ),
-        ),
-        const Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: EdgeInsets.only(left: 25, top: 10),
-              child: Text(
-                'Más productos',
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Color(0xFF464646),
-                  fontWeight: FontWeight.w500,
-                  fontFamily: 'FiraSansCondensed',
-                  letterSpacing: 3,
+          const Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: 25, top: 20),
+                child: Text(
+                  'Más productos',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Color(0xFF464646),
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'FiraSansCondensed',
+                    letterSpacing: 3,
+                  ),
                 ),
-              ),
-            )),
-        Expanded(
-          child: ListView.builder(
+              )),
+          ListView.builder(
             itemCount: 5,
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
             itemBuilder: (BuildContext context, int index) {
               return const Padding(
                 padding: EdgeInsets.all(2.0),
@@ -185,9 +186,8 @@ class _StoreUserPageState extends State<StoreUserPage> {
               );
             },
           ),
-        ),
-
-      ]),
+        ]),
+      ),
     );
   }
 }
